c1117de00da33040a2b6306a0fac42e104c72dcf
Alexandre BENARD <alben59dk@gmail.com>
2018-04-23 11:22:14 +0200

    correction

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,29 @@
-let express = require('express')
-let router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
-//--> une route (/users/[prénom]) acceptant le GET pour afficher le formulaire de update-user.pug
+//une route (/users/[prénom]) acceptant le GET pour afficher le formulaire de update-user.pug
+router.get('/:user(\[a-z]+)', (req, res, next) => {
+  res.render('update-user', {user: req.params.user});
+});
 
-router.get('/:prenom([\\a-zA-Z]+)', function (req, res, next) {
-  res.render('update-user')
-})
+//une route (/users/[prénom]) acceptant le PUT et prenant en paramètre dans l'URL une chaîne de caractères, qui affichera The new name is [prénom]
+router.put('/:user(\[a-z]+)', (req, res, next) => {
+  res.send('My name is ' + req.params.user);
+});
 
-// --> une route (/users/[prénom]) acceptant le PUT et prenant en paramètre dans l'URL une chaîne de caractères, qui affichera The new name is [prénom]
+//une route (/users/[numéro]) acceptant le GET pour afficher le formulaire de delete-user.pug
+router.get('/:id(\\d+)', (req, res, next) => {
+  res.render('delete-user', {id: req.params.id});
+});
 
-router.put('/:prenom', function(req, res, next) {
-  res.send('The new name is ' + req.body.prenom)
-})
+//une route (/users/[numéro]) acceptant le DELETE et prenant en paramètre dans l'URL un entier, qui affichera No more user with id [numéro]
+router.delete('/:id(\\d+)', (req, res, next) => {
+  res.send('No more user with id ' + req.params.id);
+});
 
-// //--> une route (/users/[numéro]) acceptant le GET pour afficher le formulaire de delete-user.pug
-
-router.get('/:numero([\\d]+)', function (req, res, next) {
-  res.render('delete-user')
-})
-
-// // -- > une route (/users/[numéro]) acceptant le DELETE et prenant en paramètre dans l'URL un entier, qui affichera No more user with id [numéro]
-
-router.delete('/:numero', function (req, res, next) {
-  res.send('No more users with id ' +  req.body.numero)
-})
-
-module.exports = router
+module.exports = router;
